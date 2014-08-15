6d468fc96da242c463ad74d56e3ee98611f228f8
kohkimakimoto <kohki.makimoto@gmail.com>
2014-08-16 07:02:14 +0900

    update test

--- tests/Kohkimakimoto/ValidatorExtension/ValidatorTest.php
+++ tests/Kohkimakimoto/ValidatorExtension/ValidatorTest.php
@@ -27,6 +27,31 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($v->passes());
     }
 
+    public function testFilters()
+    {
+        $v = new Validator(
+            $this->getRealTranslator(), 
+            array('foo' => 'aaa', 'bar' => 'bbb'), 
+            array());
+        $v->rule('foo', 'required');
+        $v->rule('bar', 'required');
+        $v->beforeFilter(function($v){
+            $foo = $v->get('foo');
+            $foo .= "foo";
+            $v->set('foo', $foo);
+        });
+
+        $v->afterFilter(function($v){
+            $bar = $v->get('bar');
+            $bar .= "bar";
+            $v->set('bar', $bar);
+        });
+
+        $this->assertTrue($v->passes());
+        $this->assertEquals("aaafoo", $v->get('foo'));
+        $this->assertEquals("bbbbar", $v->get('bar'));
+    }
+
     /**
      * https://github.com/laravel/framework/blob/4.2/tests/Validation/ValidationValidatorTest.php#L1276
      */
